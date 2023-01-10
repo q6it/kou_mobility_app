3f42151105b0c5aa848a9e6d2ec28911707429dc
Andrei Sittsenko <andrei.sittsenko@gmail.com>
2023-01-10 05:48:06 +0200

    Add auth feature

---
+++ src/features/auth/Auth.ts
@@ -0,0 +1,38 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signOut,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+import { UserCredentials } from './types';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth();
+
+export const createUser = ({ email, password }: UserCredentials) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInUser = ({ email, password }: UserCredentials) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = () => {
+    return signOut(auth);
+};
